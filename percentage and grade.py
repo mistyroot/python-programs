34390ca671457d33d416ca23077335e44af7326d
Vinayak Yadav <168169129+vinayaky7@users.noreply.github.com>
2024-08-11 23:54:33 +0530

    add

---
+++ percentage and grade.py
@@ -0,0 +1,10 @@
+#write a program to accept percentage from user and display grades according to criteria
+p=float(input("Enter percentge"))
+if p>90:
+    print("A")
+elif p>80 <=90:
+    print("B")
+elif p>60<=80:
+    print("C")
+else:
+    print("D")
