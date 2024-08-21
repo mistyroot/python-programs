0b833c2b2f774c1cf4b18556dbebb11faa008ef5
Vinayak Yadav <168169129+vinayaky7@users.noreply.github.com>
2024-08-21 14:57:53 +0530

    add

---
+++ pattern3.py
@@ -0,0 +1,14 @@
+# 11111
+# 00000
+# 11111
+# 00000
+# 11111
+
+for i in range(1,6):
+    for j in range(1,6):
+        if i%2==0:
+            print("0", end='')
+        else:
+            print("1",end='')
+    print()
+
