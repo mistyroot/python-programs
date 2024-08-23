db5f931c23924b29b7fed8bf6d0df72a3d6374ab
Vinayak Yadav <168169129+vinayaky7@users.noreply.github.com>
2024-08-23 07:11:29 +0530

    add

---
+++ pattern25.py
@@ -0,0 +1,24 @@
+#       *  
+#      * * 
+#     *   *
+#    *     *
+#   *       *
+
+space=4
+for i in range (1,6):
+    for s in range (space):
+        print(" ", end='')
+    for j in range(1,i+1):
+        print("*", end='')
+    print()
+    space=space-1
+
+space=0
+
+for i in range(5,0,-1):
+    for s in range(space):
+        print(" ", end='')
+    for j in range(1,i+1):
+        print("*", end='')
+    print()
+    space=space+1
