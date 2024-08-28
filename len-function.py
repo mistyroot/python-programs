662d89f4aabe8b52ce03e09160387e5c70c696d5
Vinayak Yadav <168169129+vinayaky7@users.noreply.github.com>
2024-08-29 03:19:01 +0530

    add

---
+++ len-function.py
@@ -0,0 +1,10 @@
+student_names=["Naresh","Suresh","Ramesh","Kishore","Rajesh","Kiran","Raman"]
+index= int(input("Enter student position to read:"))
+
+if index>= 0 and index< len(student_names) or (index >= -len(student_names)) and index <= -1 :
+    name= student_names[index]
+    print(f'Student_name {name}')
+else:
+    print("invalid index")
+
+    
