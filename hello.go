7728f49272deb128d83f959ed1e62efffbc28601
Vinayak Yadav <168169129+vinayaky7@users.noreply.github.com>
2024-10-07 00:22:27 +0530

    add

--- hello.go
+++ hello.go
@@ -4,4 +4,11 @@ import "fmt"
 
 func main() {
 	fmt.Println("hello world")
+	fmt.Println("hello world")
+	fmt.Println("hello world")
+	fmt.Println("hello world")
+	fmt.Println("hello world")
+	fmt.Println("hello world")
+	fmt.Println("hello world")
+	fmt.Println("hello world")
 }
