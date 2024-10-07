50d262ca618a311b5f8104afffd4fffdadd9e21d
Vinayak Yadav <168169129+vinayaky7@users.noreply.github.com>
2024-10-07 15:59:13 +0530

    add

---
+++ importlib_use.py
@@ -0,0 +1,8 @@
+# Write a program to import any library
+
+import importlib
+
+module_name=input("Enter Module Name or Library Name to Import")
+m=importlib.import_module(module_name)
+list1=dir(m)
+print(list1)
