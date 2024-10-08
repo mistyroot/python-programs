129309a4ec00d03194122886b385126a6a0d3940
Vinayak Yadav <168169129+vinayaky7@users.noreply.github.com>
2024-10-08 11:08:27 +0530

    addadd

--- importlib_use.py
+++ importlib_use.py
@@ -2,11 +2,9 @@
 
 import importlib
 
-<<<<<<< HEAD
 
-=======
->>>>>>> 50d262ca618a311b5f8104afffd4fffdadd9e21d
 module_name=input("Enter Module Name or Library Name to Import")
 m=importlib.import_module(module_name)
 list1=dir(m)
 print(list1)
+
